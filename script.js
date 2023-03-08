c2fef2fe18fdec6ca74f39ae820d6436c9a47cbc
Danielc3388 <102593818+Danielc3388@users.noreply.github.com>
2023-03-08 17:58:08 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	if (username === "admin" && password === "password") {
+		window.location.href = "home.html";
+	} else {
+		alert("Invalid login credentials. Please try again.");
+	}
+}
