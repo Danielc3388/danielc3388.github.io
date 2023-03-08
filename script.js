3b3dcb385acea6bac941d885fe45e818a0bfb3ab
Danielc3388 <102593818+Danielc3388@users.noreply.github.com>
2023-03-08 18:09:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function login() {
+function login(event) {
+	event.preventDefault();
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 
 	if (username === "admin" && password === "password") {
 		window.location.href = "home.html";
-		href="home.html"
 	} else {
 		alert("Invalid login credentials. Please try again.");
 	}
